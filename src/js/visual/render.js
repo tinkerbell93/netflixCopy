25b9631518e4527f3057cffb69676534ed3db735
clowns123 <clonws1232@gmail.comm>
2020-06-23 14:20:25 +0900

    dev : viusual render -> contentBrief change

---
+++ src/js/visual/render.js
@@ -0,0 +1,53 @@
+// api
+import tmdb from '../api/tmdb';
+
+// visualDetails DOMs
+const mainItemTitle = document.querySelector('.main-item-title');
+const visualWrapper = document.querySelector('.visual-wrapper');
+const visualDetails = document.querySelector('.visual-details');
+const mainItemBrief = visualDetails.querySelector('.main-item-brief');
+const maturityRating = visualDetails.querySelector('.maturity-rating');
+const linkWrapper = visualDetails.querySelector('.link-wrapper');
+
+// stats
+let content;
+let contentTitle;
+let contentBrief;
+let contentImage;
+// event
+
+// function
+async function getMainContent() {
+  let moveList = await tmdb().popularMovie();
+  let tvList = await tmdb().popularTv();
+  let allList = [...moveList, ...tvList];
+  let ranNum = Math.floor(Math.random() * (39 - 0));
+  return allList[ranNum];
+}
+
+async function getContentImage() {
+  let imageUrl = await tmdb().backdropImage([content]);
+  return imageUrl;
+}
+
+async function render() {
+  // state set
+  content = await getMainContent();
+  contentTitle = content.title ? content.title : content.name;
+  contentBrief = content.overview;
+  contentImage = await getContentImage();
+  console.log(contentImage[0]);
+  console.log(content);
+
+  // DOM change
+  mainItemTitle.textContent = contentTitle;
+  mainItemBrief.textContent = contentBrief;
+  visualWrapper.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 70px, rgba(20, 20, 20, 1) 90vh),
+  url(${contentImage[0]})`;
+}
+
+function init() {
+  render();
+}
+
+init();
