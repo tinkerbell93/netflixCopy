ab8b6f75d14306ebbac8011f2a291d64d057fe5a
clowns123 <clonws1232@gmail.comm>
2020-06-24 14:44:42 +0900

    deve: main-visual end

--- src/js/api/tmdb.js
+++ src/js/api/tmdb.js
@@ -1,111 +1,145 @@
-export default function tmdbApi() {
-  const LANGUAGE = '&language=ko-kr';
+export default function tmdbApi () {
+  const LANGUAGE = '&language=ko-kr'
   // const LANGUAGE = '';
-  const BASEURL = 'https://api.themoviedb.org/3';
-  const IMAGEURL = 'https://image.tmdb.org/t/p';
-  const APIKEY = 'api_key=250ed9163433644ad57f1350029b12e8';
-  const REGION = '&region=kr';
-  const WIDTH = 1280;
+  const BASEURL = 'https://api.themoviedb.org/3'
+  const IMAGEURL = 'https://image.tmdb.org/t/p'
+  const APIKEY = 'api_key=250ed9163433644ad57f1350029b12e8'
+  const REGION = '&region=kr'
+  const WIDTH = 1280
 
-  const MOVIE = 'movie';
-  const TV = 'tv';
-  const POPULAR = 'popular';
-  const RATED = 'top_rated';
+  const MOVIE = 'movie'
+  const TV = 'tv'
+  const POPULAR = 'popular'
+  const RATED = 'top_rated'
   // 인기 티비
-  async function popularTv(num = 1) {
-    let fullUrl = `${BASEURL}/${TV}/${POPULAR}?${APIKEY}${LANGUAGE}&page=${num}`;
-    let tvLists = {};
-    let getList = await fetch(fullUrl);
-    tvLists = await getList.json().then((list) => list.results);
-    return tvLists;
+  async function popularTv (num = 1) {
+    let fullUrl = `${BASEURL}/${TV}/${POPULAR}?${APIKEY}${LANGUAGE}&page=${num}`
+    let tvLists = {}
+    let getList = await fetch(fullUrl)
+    tvLists = await getList.json().then(list => list.results)
+    return tvLists
   }
   // 인기 영화
-  async function popularMovie(num = 1) {
-    let fullUrl = `${BASEURL}/${MOVIE}/${POPULAR}?${APIKEY}${LANGUAGE}&page=${num}${REGION}`;
-    let tvLists = {};
-    let getList = await fetch(fullUrl);
-    tvLists = await getList.json().then((list) => list.results);
-    return tvLists;
+  async function popularMovie (num = 1) {
+    let fullUrl = `${BASEURL}/${MOVIE}/${POPULAR}?${APIKEY}${LANGUAGE}&page=${num}${REGION}`
+    let tvLists = {}
+    let getList = await fetch(fullUrl)
+    tvLists = await getList.json().then(list => list.results)
+    return tvLists
   }
   // 탑 티비
-  async function ratedTv(num = 1) {
-    let fullUrl = `${BASEURL}/${TV}/${RATED}?${APIKEY}${LANGUAGE}&page=${num}`;
-    let tvLists = {};
-    let getList = await fetch(fullUrl);
-    tvLists = await getList.json().then((list) => list.results);
-    return tvLists;
+  async function ratedTv (num = 1) {
+    let fullUrl = `${BASEURL}/${TV}/${RATED}?${APIKEY}${LANGUAGE}&page=${num}`
+    let tvLists = {}
+    let getList = await fetch(fullUrl)
+    tvLists = await getList.json().then(list => list.results)
+    return tvLists
   }
   // 탑 영화
-  async function ratedMovie(num = 1) {
-    let fullUrl = `${BASEURL}/${MOVIE}/${RATED}?${APIKEY}${LANGUAGE}&page=${num}${REGION}`;
-    let tvLists = {};
-    let getList = await fetch(fullUrl);
-    tvLists = await getList.json().then((list) => list.results);
-    return tvLists;
+  async function ratedMovie (num = 1) {
+    let fullUrl = `${BASEURL}/${MOVIE}/${RATED}?${APIKEY}${LANGUAGE}&page=${num}${REGION}`
+    let tvLists = {}
+    let getList = await fetch(fullUrl)
+    tvLists = await getList.json().then(list => list.results)
+    return tvLists
   }
   // 백그라운드 이미지
-  async function backdropImage(lists = []) {
-    lists = lists.map((test) => test.backdrop_path);
-    let imageList = lists.map((list) => `${IMAGEURL}/w${WIDTH}${list}`);
-    return imageList;
+  async function backdropImage (lists = []) {
+    lists = lists.map(test => test.backdrop_path)
+    let imageList = lists.map(list => `${IMAGEURL}/w${WIDTH}${list}`)
+    return imageList
   }
   // 포스터 이미지
-  async function posterImage(lists = []) {
-    lists = lists.map((test) => test.poster_path);
-    let imageList = lists.map((list) => `${IMAGEURL}/w${WIDTH}${list}`);
-    return imageList;
+  async function posterImage (lists = []) {
+    lists = lists.map(test => test.poster_path)
+    let imageList = lists.map(list => `${IMAGEURL}/w${WIDTH}${list}`)
+    return imageList
   }
   // 스틸컷 이미지
-  async function stillImage(lists = []) {
-    lists = lists.map((test) => test.still_path);
-    let imageList = lists.map((list) => `${IMAGEURL}/w${WIDTH}${list}`);
-    return imageList;
+  async function stillImage (lists = []) {
+    lists = lists.map(test => test.still_path)
+    let imageList = lists.map(list => `${IMAGEURL}/w${WIDTH}${list}`)
+    return imageList
   }
 
   // 검색
-  async function searchAll(query = '') {
-    let fullUrl = `${BASEURL}/search/multi?${APIKEY}${LANGUAGE}&query=${query}&page=1&include_adult=false${REGION}`;
-    return fullUrl;
+  async function searchAll (query = '') {
+    let fullUrl = `${BASEURL}/search/multi?${APIKEY}${LANGUAGE}&query=${query}&page=1&include_adult=false${REGION}`
+    return fullUrl
   }
   // 티비 에피소드
-  async function getTvEpisodes(tvObj = {}) {
-    let fullUrl = `${BASEURL}/${TV}/${tvObj.id}/season/1?${APIKEY}${LANGUAGE}`;
-    let epLists = {};
-    let getEpList = await fetch(fullUrl);
-    epLists = await getEpList.json().then((list) => list.episodes);
-    return epLists;
+  async function getTvEpisodes (tvObj = {}) {
+    let fullUrl = `${BASEURL}/${TV}/${tvObj.id}/season/1?${APIKEY}${LANGUAGE}`
+    let epLists = {}
+    let getEpList = await fetch(fullUrl)
+    epLists = await getEpList.json().then(list => list.episodes)
+    return epLists
   }
 
   // 비슷한 목록
-  async function getSmilarList(kind, id) {
-    let fullUrl = `${BASEURL}/${kind}/${id}/similar?${APIKEY}${LANGUAGE}&page=1`;
-    let smilarLists = {};
-    let getList = await fetch(fullUrl);
-    smilarLists = await getList.json().then((list) => list.results);
-    return smilarLists;
+  async function getSmilarList (kind, id) {
+    let fullUrl = `${BASEURL}/${kind}/${id}/similar?${APIKEY}${LANGUAGE}&page=1`
+    let smilarLists = {}
+    let getList = await fetch(fullUrl)
+    smilarLists = await getList.json().then(list => list.results)
+    return smilarLists
   }
 
   // 종류
-  async function getGnere() {
-    let fullUrl = `${BASEURL}/genre/${MOVIE}/list?${APIKEY}${LANGUAGE}`;
-    let getList = await fetch(fullUrl);
+  async function getGnere () {
+    let fullUrl = `${BASEURL}/genre/${MOVIE}/list?${APIKEY}${LANGUAGE}`
+    let getList = await fetch(fullUrl)
     let getGnere = []
-    let temp = [];
-    getGnere = await getList.json().then((list) => list.genres);
+    let temp = []
+    getGnere = await getList.json().then(list => list.genres)
 
-    fullUrl = `${BASEURL}/genre/${TV}/list?${APIKEY}${LANGUAGE}`;
-    getList = await fetch(fullUrl);
+    fullUrl = `${BASEURL}/genre/${TV}/list?${APIKEY}${LANGUAGE}`
+    getList = await fetch(fullUrl)
 
-    temp = await getList.json().then((list) => list.genres);
-    
+    temp = await getList.json().then(list => list.genres)
+
     getGnere = [...getGnere, ...temp]
-    getGnere = getGnere.filter((thing,index) => {
-      return index === getGnere.findIndex(obj => {
-        return JSON.stringify(obj) === JSON.stringify(thing);
-      });
-    });
+    getGnere = getGnere.filter((thing, index) => {
+      return (
+        index ===
+        getGnere.findIndex(obj => {
+          return JSON.stringify(obj) === JSON.stringify(thing)
+        })
+      )
+    })
     return getGnere
-}
+  }
+
+  async function detailMovie (id) {
+    let fullUrl = `${BASEURL}/${MOVIE}/${id}?${APIKEY}${LANGUAGE}`
+    let movieDetail = {}
+    let getDetail = await fetch(fullUrl)
+    movieDetail = await getDetail.json().then(list => list)
+    return movieDetail
+  }
+  async function detailTv (id) {
+    let fullUrl = `${BASEURL}/${TV}/${id}?${APIKEY}${LANGUAGE}`
+    let tvDetail = {}
+    let getDetail = await fetch(fullUrl)
+    tvDetail = await getDetail.json().then(list => list)
+    return tvDetail
+  }
+
+  // 배우
+  async function castMovie (id) {
+    let fullUrl = `${BASEURL}/${MOVIE}/${id}/credits?${APIKEY}${LANGUAGE}`
+    let movieCast = {}
+    let getCast = await fetch(fullUrl)
+    movieCast = await getCast.json().then(list => list)
+    return movieCast
+  }
+  async function castTv (id) {
+    let fullUrl = `${BASEURL}/${TV}/${id}/credits?${APIKEY}${LANGUAGE}`
+    let tvCast = {}
+    let getCast = await fetch(fullUrl)
+    tvCast = await getCast.json().then(list => list)
+    return tvCast
+  }
 
   return {
     popularTv,
@@ -119,5 +153,16 @@ export default function tmdbApi() {
     stillImage,
     getSmilarList,
     getGnere,
-  };
+    detailMovie,
+    detailTv,
+    castMovie,
+    castTv
+  }
 }
+
+// async function init () {
+//   let test = tmdbApi()
+//   console.log(await test.detailTv('12637'))
+// }
+
+// init()
--- src/js/visual/render.js
+++ src/js/visual/render.js
@@ -1,14 +1,16 @@
 // api
-import tmdb from '../api/tmdb';
+import tmdb from '../api/tmdb'
 
 // visualDetails DOMs
-const mainItemTitle = document.querySelector('.main-item-title');
-const visualWrapper = document.querySelector('.visual-wrapper');
-const visualDetails = document.querySelector('.visual-details');
-const mainItemBrief = visualDetails.querySelector('.main-item-brief');
-const infoMoreBtn = document.querySelector('.infoMore-btn');
+const mainVisual = document.querySelector('.main-visual')
+const mainItemTitle = document.querySelector('.main-item-title')
+const visualWrapper = document.querySelector('.visual-wrapper')
+const visualDetails = document.querySelector('.visual-details')
+const mainItemBrief = visualDetails.querySelector('.main-item-brief')
+const infoMoreBtn = document.querySelector('.infoMore-btn')
 // visualDetails DOMs end
 // info DOMs
+const infoMore = document.querySelector('.infoMore')
 const mainNavigation = document.querySelector('.main-navigation')
 // overView
 const overview = mainNavigation.querySelector('.overview')
@@ -18,87 +20,121 @@ const contentInfo = overview.querySelector('.content-info')
 const year = contentInfo.querySelector('.yaer')
 const maturityRating = contentInfo.querySelector('.maturity-rating')
 const synopsis = overview.querySelector('.synopsis')
+const cast = overview.querySelector('.cast')
+const castListItems = cast.querySelector('.list-items')
+const genres = overview.querySelector('.genres')
+const genresListItems = genres.querySelector('.list-items')
 // infoMore-nav-lists-roundInfo
 const c = mainNavigation.querySelector('.infoMore-nav-lists-roundInfo')
 // infoMore-nav-lists-similarInfo
-const infoMoreNavListsSimilarInfo = mainNavigation.querySelector('.infoMore-nav-lists-similarInfo')
+const infoMoreNavListsSimilarInfo = mainNavigation.querySelector(
+  '.infoMore-nav-lists-similarInfo'
+)
 // infoMore-nav-lists-moreInfo
-const infoMoreNavListsMoreInfo = mainNavigation.querySelector('.infoMore-nav-lists-moreInfo')
+const infoMoreNavListsMoreInfo = mainNavigation.querySelector(
+  '.infoMore-nav-lists-moreInfo'
+)
 // info DOMs end
 
 // stats
-let content;
-let contentTitle;
-let contentBrief;
-let contentImage;
-let releaseDate;
-let firstAirDate;
+let content
+let contentTitle
+let contentBrief
+let contentImage
+let releaseDate
+let firstAirDate
+let ranNum
+let castView
 // event
 
 // function
-async function getMainContent() {
-  let moveList = await tmdb().popularMovie();
-  let tvList = await tmdb().popularTv();
-  let allList = [...moveList, ...tvList];
-  let ranNum = Math.floor(Math.random() * (39 - 0));
-  return allList[ranNum];
+async function getMainContent () {
+  let moveList = await tmdb().popularMovie()
+  let tvList = await tmdb().popularTv()
+  let allList = [...moveList, ...tvList]
+  ranNum = Math.floor(Math.random() * (39 - 0))
+  return allList[ranNum]
 }
 
-async function getContentImage() {
-  let imageUrl = await tmdb().backdropImage([content]);
-  return imageUrl;
+async function getContentImage () {
+  let imageUrl = await tmdb().backdropImage([content])
+  return imageUrl
 }
 
 // main-visual render
-async function render() {
+async function render () {
   // state set
-  content = await getMainContent();
-  contentTitle = content.title ? content.title : content.name;
+  content = await getMainContent()
+  contentTitle = content.title ? content.title : content.name
 
-  contentBrief = content.overview;
-  let temp = contentBrief.split(' ');
+  contentBrief = content.overview
+  let temp = contentBrief.split(' ')
   if (temp.length > 30) {
-    temp = contentBrief.split('.');
-    contentBrief = temp[0] + '.';
+    temp = contentBrief.split('.')
+    contentBrief = temp[0] + '.'
   }
 
   if (content.adult !== undefined || content.adult !== false)
-    document.querySelector('.maturity-rating').style.opacity = 0;
+    document.querySelector('.maturity-rating').style.opacity = 0
 
-  contentImage = await getContentImage();
+  contentImage = await getContentImage()
 
   // DOM change
-  mainItemTitle.textContent = contentTitle;
-  mainItemBrief.textContent = contentBrief;
-  visualWrapper.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 70px, rgba(20, 20, 20, 1) 90vh), url(${contentImage[0]})`;
+  mainItemTitle.textContent = contentTitle
+  mainItemBrief.textContent = contentBrief
+  visualWrapper.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 70px, rgba(20, 20, 20, 1) 90vh), url(${contentImage[0]})`
 }
 // main-visual render end
 
 // info
-function infoOverview(){
+async function infoOverview () {
+  mainVisual.style.display = 'none'
+  infoMore.style.display = 'block'
   // year
-  releaseDate = content.release_date;
+  releaseDate = content.release_date
   firstAirDate = content.first_air_date
-  let yearNum = releaseDate ? releaseDate : firstAirDate;
+  let yearNum = releaseDate ? releaseDate : firstAirDate
   yearNum = yearNum.split('-')[0]
 
-  // adult 
-  let adultDate = content.adult ? "청소년 관람 불과" : ''
-  
-  
+  // adult
+  let adultDate = content.adult ? '청소년 관람 불가' : ''
+  let detailView
+  // detail
+  if (ranNum < 20) {
+    detailView = await tmdb().detailMovie(content.id)
+    castView = await tmdb().castMovie(content.id)
+    castView = castView.cast.map(({ name }) => name)
+  } else {
+    detailView = await tmdb().detailTv(content.id)
+    castView = await tmdb().castTv(content.id)
+    castView = castView.cast.map(({ name }) => name)
+  }
+  castView = castView.slice(0, 3)
+
+  let genres = await detailView.genres
+  console.log(genres)
+
   // render
+  infoMoreBg.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 70px, rgba(20, 20, 20, 1) 90vh), url(${contentImage[0]})`
+  year.textContent = yearNum + '년'
   infoTitle.textContent = contentTitle
-  infoMoreBg.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0) 70px, rgba(20, 20, 20, 1) 90vh), url(${contentImage[0]})`;
-  year.textContent = yearNum
   maturityRating.textContent = adultDate
   synopsis.textContent = contentBrief
+  castView.forEach(cast => {
+    let html = `<li>${cast}<li>`
+    castListItems.innerHTML += html
+  })
+
+  genres.forEach(a => {
+    let html = `<li>${a.name}</li>`
+    genresListItems.innerHTML += html
+  })
 }
 
-async function init() {
-  await render();
+async function init () {
+  await render()
   infoMoreBtn.addEventListener('click', infoOverview)
-  console.log(content);
-  
+  console.log(content)
 }
 
-init();
+init()
